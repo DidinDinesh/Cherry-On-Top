4afc7ae61ccb42f64778dd8c44fca3cc9ddc2a79
DidinDinesh <155463888+DidinDinesh@users.noreply.github.com>
2024-09-25 12:32:39 +0530

    small changes

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,7 @@
 
 import admin from 'firebase-admin';
+import serviceAccount from '../google_config/cherry-on-top-590e0-firebase-adminsdk-r7ls2-a00306ab43.json'
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
