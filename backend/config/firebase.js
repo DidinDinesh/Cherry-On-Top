5826f4dad03c8f10022d785fc06f5d22f0a96306
DidinDinesh <155463888+DidinDinesh@users.noreply.github.com>
2024-09-25 11:28:40 +0530

    small change

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 import admin from 'firebase-admin';
-import serviceAccount from './cherry-on-top-590e0-firebase-adminsdk-r7ls2-a00306ab43.json'; // Download from Firebase console
+import serviceAccount from '../google-service-acc/cherry-on-top-590e0-firebase-adminsdk-r7ls2-a00306ab43.json'; // Download from Firebase console
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
